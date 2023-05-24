bedfe8b7ac03c218893b07297816669ae7323f9f
aramoh3ni <alireza.mohseni.se@gmail.com>
2023-05-24 21:42:32 +0430

    prevent from extra feild from req body

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // allow validation of requests
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // allow us to get only the necessary params from url
+    }),
+  ); // allow validation of requests
   await app.listen(8000, "0.0.0.0");
 }
 bootstrap();
