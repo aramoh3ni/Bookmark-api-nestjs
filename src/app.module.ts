a1f70be0c4e0a31caf7948c67f87a7aa3f3ff9b3
aramoh3ni <alireza.mohseni.se@gmail.com>
2023-05-24 20:16:13 +0430

    make single place import from @Global Decorator.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "./Auth/auth.module";
 import { UserModule } from "./user/user.module";
 import { BookmarkModule } from "./bookmark/bookmark.module";
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
   imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
