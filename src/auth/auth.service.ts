eb48039e30d62431ecdd4302b8fa922066db46bf
aramoh3ni <alireza.mohseni.se@gmail.com>
2023-05-24 20:12:57 +0430

    adding module and exporting services throught each module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   login(): { message: string } {
     return {
       message: "I am Successfully Logged in",
