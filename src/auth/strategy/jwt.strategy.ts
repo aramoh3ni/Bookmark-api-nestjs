4e29ea529946d0848423ed605ae2e377e9e0240c
aramoh3ni <alireza.mohseni.se@gmail.com>
2023-05-26 14:27:14 +0430

    make routes protected.

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -4,12 +4,16 @@ import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
   constructor(config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: config.get("JWT_SECRET_KEY"),
     });
+  }
+
+  validate(payload: any) {
+    return payload;
   }
 }
---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Req, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { Request } from "express";
+// import { User } from "@prisma/client";
+
+@Controller("users")
+export class UserController {
+  @UseGuards(AuthGuard("jwt"))
+  @Get("me")
+  getMe(@Req() req: Request): string {
+    return "my user";
+  }
+}
