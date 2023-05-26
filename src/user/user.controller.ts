a63b210b3c0a32ce33d4e01f7c48678600415ce1
aramoh3ni <alireza.mohseni.se@gmail.com>
2023-05-26 14:46:46 +0430

    feature - adding custom guard to prevent untrasted access.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Req, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
 import { Request } from "express";
+import { JwtGuard } from "src/auth/guard";
 // import { User } from "@prisma/client";
 
 @Controller("users")
 export class UserController {
-  @UseGuards(AuthGuard("jwt"))
+  @UseGuards(JwtGuard)
   @Get("me")
   getMe(@Req() req: Request): string {
     console.log(req.user);
